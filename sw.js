606f2ca80e7e90852785de81925d203cd9b3a899
Sangeeth995 <52905761+Sangeeth995@users.noreply.github.com>
2026-01-11 19:33:40 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+// If you ever update the app and it doesn't change, bump v1 -> v2 -> v3...
+const CACHE_NAME = "lifebank-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
